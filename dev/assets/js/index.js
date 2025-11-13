b83db6354ee70001beb2e090c596e9106b95d7da
StupakYuliia <yliastupac25@gmail.com>
2025-11-13 21:30:50 +0100

    section testimonials is ready

--- dev/assets/js/index.js
+++ dev/assets/js/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import * as ReactDOMClient from "react-dom/client"
 import App from './App'
+import ContentBlock from '../../components/ContentBlock'
 import {initAppLogic} from "./init"
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -8,4 +9,20 @@ document.addEventListener('DOMContentLoaded', () => {
     app.render(<App/>)
 
 	initAppLogic();
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+	const testimonialsRroot = ReactDOMClient.createRoot(document.getElementById("testimonials-cards"))
+	testimonialsRroot.render(<ContentBlock
+		title="Real Stories from Real Customers"
+		text="Get inspired by these stories."
+	/>)
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+	const testimonialsRroot = ReactDOMClient.createRoot(document.getElementById("testimonials-logos"))
+	testimonialsRroot.render(<ContentBlock
+		title="We serve over 100 Nigerian Websites"
+		text="Connect LemonWares with your favourite tools that you use daily and keep things on track."
+	/>)
 })
